faa522b11dbfc42e437fb3130238ae239d68a360
4ndrisw <4ndr1sw@gmail.com>
2023-05-09 14:30:02 +0700

    Update routes.php

--- config/routes.php
+++ config/routes.php
@@ -9,5 +9,7 @@ $route['billings/billing/(:num)/(:any)'] = 'billing/index/$1/$2';
  */
 $route['billings/list'] = 'mybilling/list';
 $route['billings/show/(:num)/(:any)'] = 'mybilling/show/$1/$2';
-$route['billings/office/(:num)/(:any)'] = 'mybilling/office/$1/$2';
 $route['billings/pdf/(:num)'] = 'mybilling/pdf/$1';
+$route['billings/taggable_pdf/(:num)'] = 'mybilling/taggable_pdf/$1';
+
+
